b078dcb6dd9c582950f112ec56528df382be6ad7
Jose <josemsg@gmail.com>
2018-05-29 09:20:28 +0200

    Añadido sistema de depuración

--- admin/controller.php
+++ admin/controller.php
@@ -7,23 +7,38 @@ class LibrosController extends JControllerLegacy
 
 	public function display($cachable = false, $urlparams = false)
 	{
-		require_once JPATH_COMPONENT.'/helpers/libro.php';
+		
+            require_once JPATH_COMPONENT.'/helpers/libro.php';
+                
+            $this->mostrarZona(__CLASS__,__METHOD__);
+            
+            $view   = $this->input->get('view', 'libros');
+            $layout = $this->input->get('layout', 'default');
+            $id     = $this->input->getInt('id');
 
-		$view   = $this->input->get('view', 'libros');
-		$layout = $this->input->get('layout', 'default');
-		$id     = $this->input->getInt('id');
+            if ($view == 'libro' && $layout == 'edit' && !$this->checkEditId('com_libros.edit.libro', $id))
+            {
+                    $this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
+                    $this->setMessage($this->getError(), 'error');
+                    $this->setRedirect(JRoute::_('index.php?option=com_libros&view=libros', false));
 
-		if ($view == 'libro' && $layout == 'edit' && !$this->checkEditId('com_libros.edit.libro', $id))
-		{
-			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
-			$this->setMessage($this->getError(), 'error');
-			$this->setRedirect(JRoute::_('index.php?option=com_libros&view=libros', false));
+                    return false;
+            }
 
-			return false;
-		}
+            parent::display();
 
-		parent::display();
-
-		return $this;
+            return $this;
+            
 	}
+        
+        function mostrarZona($clase,$metodo){
+        
+        if (JRequest::getVar( 'DEBUG') == JRequest::getVar( 'ACTIVAR_DEBUG')) {
+            echo "------------------------- <br> ";
+            echo "Clase: ". $clase ." <br>";
+            echo "Método: ". $metodo ." <br>";
+            echo "------------------------- <br> ";
+        }
+        
+    }
 }
--- admin/libros.php
+++ admin/libros.php
@@ -6,6 +6,10 @@ if (!JFactory::getUser()->authorise('core.manage', 'com_libros'))
 	return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
+JRequest::setVar( 'DEBUG', "SI") ;
+JRequest::setVar( 'ACTIVAR_DEBUG', "SI") ;
+//JRequest::setVar( 'ACTIVAR_DEBUG', "NO") ;
+
 $controller	= JControllerLegacy::getInstance('Libros');
 $controller->execute(JFactory::getApplication()->input->get('task'));
 $controller->redirect();
