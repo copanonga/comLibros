94158e390f0c61a918415d9316723f6347d1a643
Jose <josemsg@gmail.com>
2018-05-29 09:09:27 +0200

    Creado controlador

---
+++ admin/controller.php
@@ -0,0 +1,29 @@
+<?php
+defined('_JEXEC') or die;
+
+class LibrosController extends JControllerLegacy
+{
+	protected $default_view = 'libros';
+
+	public function display($cachable = false, $urlparams = false)
+	{
+		require_once JPATH_COMPONENT.'/helpers/libro.php';
+
+		$view   = $this->input->get('view', 'libros');
+		$layout = $this->input->get('layout', 'default');
+		$id     = $this->input->getInt('id');
+
+		if ($view == 'libro' && $layout == 'edit' && !$this->checkEditId('com_libros.edit.libro', $id))
+		{
+			$this->setError(JText::sprintf('JLIB_APPLICATION_ERROR_UNHELD_ID', $id));
+			$this->setMessage($this->getError(), 'error');
+			$this->setRedirect(JRoute::_('index.php?option=com_libros&view=libros', false));
+
+			return false;
+		}
+
+		parent::display();
+
+		return $this;
+	}
+}
