61cf5be9bc2f3511883ee19a0db5b37c7c4157dc
Jose <josemsg@gmail.com>
2018-05-30 10:54:54 +0200

    Simplificada view para crear y editar

--- admin/views/libro/tmpl/edit.php
+++ admin/views/libro/tmpl/edit.php
@@ -4,24 +4,34 @@ defined('_JEXEC') or die;
 ?>
 
 <form action="<?php echo JRoute::_('index.php?option=com_libros&layout=edit&id='.(int) $this->item->id); ?>" method="post" name="adminForm" id="adminForm" class="form-validate">
-	<div class="row-fluid">
-		<div class="span10 form-horizontal">
+    <div class="row-fluid">
+        <div class="span10 form-horizontal">
 
-	<fieldset>
-		<?php echo JHtml::_('bootstrap.startPane', 'myTab', array('active' => 'details')); ?>
+            <fieldset>
+                <?php echo JHtml::_('bootstrap.startTabSet', 'myTab', array('active' => 'details')); ?>
 
-			<?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_LIBROS_NEW_LIBRO', true) : JText::sprintf('COM_LIBROS_EDIT_LIBRO', $this->item->id, true)); ?>
-				<div class="control-group">
-					<div class="control-label"><?php echo $this->form->getLabel('title'); ?></div>
-					<div class="controls"><?php echo $this->form->getInput('title'); ?></div>
-				</div>
-			<?php echo JHtml::_('bootstrap.endPanel'); ?>
+                    <?php echo JHtml::_('bootstrap.addTab', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_LIBROS_NEW_LIBRO', true) : JText::sprintf('COM_LIBROS_EDIT_LIBRO', $this->item->id, true)); ?>
+                
+                        <?php echo JHtml::_('bootstrap.addPanel', 'myTab', 'details', empty($this->item->id) ? JText::_('COM_FOLIO_NEW_FOLIO', true) : JText::sprintf('COM_FOLIO_EDIT_FOLIO', $this->item->id, true)); ?>
 
-			<input type="hidden" name="task" value="" />
-			<?php echo JHtml::_('form.token'); ?>
+                        <?php foreach ($this->form->getFieldset('myfields') as $field) : ?>
+                                <div class="control-group">
+                                        <div class="control-label">
+                                                <?php echo $field->label; ?>
+                                        </div>
+                                        <div class="controls">
+                                                <?php echo $field->input; ?>
+                                        </div>
+                                </div>
+                        <?php endforeach; ?>
 
-		<?php echo JHtml::_('bootstrap.endPane'); ?>
-		</fieldset>
-		</div>
-	</div>
+                    <?php echo JHtml::_('bootstrap.endTab'); ?>
+
+                    <input type="hidden" name="task" value="" />
+                    <?php echo JHtml::_('form.token'); ?>
+
+                <?php echo JHtml::_('bootstrap.endTabSet'); ?>
+            </fieldset>
+            </div>
+    </div>
 </form>
